de9b532e90807fa0856bb62b91cab506de2592d0
Javier Zardain <javier.a.zardain@gmail.com>
2026-09-23 20:18:20 -0300

    Ejercicio estacion de mantenimiento

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 mod parte4;
 
 use crate::parte4::recepcion::zona_recepcion;
+use crate::parte4::mantenimiento::estacion_mantenimiento;  
 
 fn main() {
+    println!("Ejercicio parte 4: Zona de recepción:");
     zona_recepcion();
+
+    println!("Ejercicio parte 4: Estación de mantenimiento:");
+    estacion_mantenimiento();
     // TODO: parte4::recepcion::simular();
     // TODO: parte4::mantenimiento::simular();
 }
--- src/parte4/mantenimiento.rs
+++ src/parte4/mantenimiento.rs
@@ -1,1 +1,102 @@
-// Parte 4: Estación de Mantenimiento (1 mecánico, 4 bahías, N robots).
+use std::collections::VecDeque;
+use std::sync::{Arc, Condvar, Mutex};
+use std::thread;
+use std::time::Duration;
+use rand::Rng;
+
+const BAHIAS_ESPERA: usize = 4;
+const TOTAL_ROBOTS: usize = 10;
+
+struct Taller {
+    esperando: VecDeque<usize>,   
+    robots_restantes: usize,  
+}
+
+pub fn estacion_mantenimiento() {
+    let taller = Arc::new((
+        Mutex::new(Taller {
+            esperando: VecDeque::new(),
+            robots_restantes: TOTAL_ROBOTS,
+        }),
+        Condvar::new()
+    ));
+
+    let taller_m = Arc::clone(&taller);
+    let mecanico = thread::spawn(move || {
+        let mut rng = rand::thread_rng();
+        let (ref lock, ref hay_robot) = *taller_m;
+
+        loop {
+            let mut state = lock.lock().unwrap();
+            
+            while state.esperando.is_empty() {
+                if state.robots_restantes == 0 {
+                    println!("Mecanico: No hay más robots para reparar, cierro el taller");
+                    return;
+                }
+                println!("Mecanico: no hay robots para arreglar, me duermo...");
+                state = hay_robot.wait(state).unwrap();
+            }
+
+            let robot_id = state.esperando.pop_front().unwrap();
+
+            println!(
+                "Mecánico: reparando al robot {}. (esperando: {})",
+                robot_id,
+                state.esperando.len()
+            );
+
+            drop(state);
+
+            let duracion = rng.gen_range(200..500);
+            thread::sleep(Duration::from_millis(duracion));
+
+            let mut state = lock.lock().unwrap();
+            state.robots_restantes -= 1;
+            println!(
+                "Mecánico: terminé con el robot {}. (restantes: {})",
+                robot_id, state.robots_restantes
+            );
+            drop(state);
+        }
+    });
+
+    let mut robots = vec![];
+    for id in 1..=TOTAL_ROBOTS {
+        let taller_r = Arc::clone(&taller);
+
+        let handle = thread::spawn(move || {
+        let mut rng = rand::thread_rng();
+        loop{
+            // Retraso en llegar de los robots
+            thread::sleep(Duration::from_millis(rng.gen_range(100..2000)));
+
+            let (ref lock, ref hay_robot) = *taller_r;
+            let mut state = lock.lock().unwrap();
+
+            println!("Robot {}: llegó al taller.", id);
+
+            if state.esperando.len() >= BAHIAS_ESPERA {
+                println!("Robot {} rechazado, vuelve a trabajar.", id);
+                //El enunciado dice que el robot "volverá más tarde", entonces vuelve a iterar el loop
+                continue;
+            }
+
+            state.esperando.push_back(id);
+            println!("Robot {}: espero en la bahía. (ocupadas: {}/{})",
+                    id, state.esperando.len(), BAHIAS_ESPERA);
+            hay_robot.notify_one();
+            drop(state);
+            break;
+        }
+        });
+        robots.push(handle);
+    }
+
+    for c in robots {
+        c.join().unwrap();
+    }
+    mecanico.join().unwrap();
+
+    println!("\nTaller cerrado. Fin del programa.");
+}
