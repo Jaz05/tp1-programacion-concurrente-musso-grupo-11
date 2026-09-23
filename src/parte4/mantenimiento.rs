fdcaa16a7a606012ed1f0e01a8abf406f3a8028e
Javier Zardain <javier.a.zardain@gmail.com>
2026-09-23 20:47:43 -0300

    Comentarios

--- src/parte4/mantenimiento.rs
+++ src/parte4/mantenimiento.rs
@@ -29,6 +29,7 @@ pub fn estacion_mantenimiento() {
         loop {
             let mut state = lock.lock().unwrap();
             
+            //El mecánico revisa las bahías de espera
             while state.esperando.is_empty() {
                 if state.robots_restantes == 0 {
                     println!("Mecanico: No hay más robots para reparar, cierro el taller");
@@ -36,8 +37,10 @@ pub fn estacion_mantenimiento() {
                 }
                 println!("Mecanico: no hay robots para arreglar, me duermo...");
                 state = hay_robot.wait(state).unwrap();
+                //Se despierta cuando hacen el notify a hay_robot
             }
 
+            //El mecánico toma uno de los robots de la bahía
             let robot_id = state.esperando.pop_front().unwrap();
 
             println!(
@@ -46,12 +49,16 @@ pub fn estacion_mantenimiento() {
                 state.esperando.len()
             );
 
+            //Libera el lock mientras arregla al robot
             drop(state);
 
+            //Simula una demora arreglando al robot
             let duracion = rng.gen_range(200..500);
             thread::sleep(Duration::from_millis(duracion));
 
             let mut state = lock.lock().unwrap();
+
+            //Un robot menos para arreglar
             state.robots_restantes -= 1;
             println!(
                 "Mecánico: terminé con el robot {}. (restantes: {})",
@@ -66,29 +73,34 @@ pub fn estacion_mantenimiento() {
         let taller_r = Arc::clone(&taller);
 
         let handle = thread::spawn(move || {
-        let mut rng = rand::thread_rng();
-        loop{
-            // Retraso en llegar de los robots
-            thread::sleep(Duration::from_millis(rng.gen_range(100..2000)));
+            let mut rng = rand::thread_rng();
+            loop{
 
-            let (ref lock, ref hay_robot) = *taller_r;
-            let mut state = lock.lock().unwrap();
+                // Retraso de los robots en llegar al taller
+                thread::sleep(Duration::from_millis(rng.gen_range(100..2000)));
 
-            println!("Robot {}: llegó al taller.", id);
+                let (ref lock, ref hay_robot) = *taller_r;
+                let mut state = lock.lock().unwrap();
 
-            if state.esperando.len() >= BAHIAS_ESPERA {
-                println!("Robot {} rechazado, vuelve a trabajar.", id);
-                //El enunciado dice que el robot "volverá más tarde", entonces vuelve a iterar el loop
-                continue;
+                println!("Robot {}: llegó al taller.", id);
+
+                //Si no hay lugar en la bahía de espera, se va y regresa luego
+                if state.esperando.len() >= BAHIAS_ESPERA {
+                    println!("Robot {} rechazado, vuelve a trabajar.", id);
+                    //El enunciado dice que el robot "volverá más tarde", entonces vuelve a iterar el loop
+                    continue;
+                }
+
+                //El robot ingresa en la bahía
+                state.esperando.push_back(id);
+                println!("Robot {}: espero en la bahía. (ocupadas: {}/{})",
+                        id, state.esperando.len(), BAHIAS_ESPERA);
+
+                //Notifica al meçánico para que se despierte
+                hay_robot.notify_one();
+                drop(state);
+                break;
             }
-
-            state.esperando.push_back(id);
-            println!("Robot {}: espero en la bahía. (ocupadas: {}/{})",
-                    id, state.esperando.len(), BAHIAS_ESPERA);
-            hay_robot.notify_one();
-            drop(state);
-            break;
-        }
         });
         robots.push(handle);
     }
@@ -98,5 +110,5 @@ pub fn estacion_mantenimiento() {
     }
     mecanico.join().unwrap();
 
-    println!("\nTaller cerrado. Fin del programa.");
+    println!("\nTaller cerrado.");
 }
--- src/parte4/recepcion.rs
+++ src/parte4/recepcion.rs
@@ -15,20 +15,20 @@ pub fn zona_recepcion() {
         Condvar::new(), // not_empty
     ));
 
+    //Agregamos una lista finita de productos entregados por los dos camiones
     let lista_productos = Arc::new(Mutex::new((VecDeque::<usize>::new(), false)));
-
     let mut lista = lista_productos.lock().unwrap();
     for i in 1..=TOTAL_ITEMS {
         lista.0.push_back(i);
     }
     drop(lista);
 
-    let mut producers = vec![];
+    let mut camiones = vec![];
     for id in 1..=2 {
         
         let buffer_prod = Arc::clone(&buffer);
         let lista_prod = Arc::clone(&lista_productos);
-        let producer = thread::spawn(move || {            
+        let camion = thread::spawn(move || {            
             let mut rng = rand::thread_rng();
             let (ref lock, ref not_full, ref not_empty) = *buffer_prod;
             
@@ -36,26 +36,30 @@ pub fn zona_recepcion() {
                 let mut lista = lista_prod.lock().unwrap();
                 let mut state = lock.lock().unwrap();
 
+                //El camión espera si la cinta está llena y no puede mandar más productos
                 while state.0.len() >= BUFFER_SIZE { 
                     println!("La cinta esta llena, el camión {} espera", id);
                     state = not_full.wait(state).unwrap(); 
                 }
 
+                //Si el camión no tiene más productos para entregar termina
                 if lista.0.is_empty(){
                     drop(lista);
                     break;
                 }
 
+                //El camión pone productos en la cinta
                 let item = lista.0.pop_back().unwrap();
                 state.0.push_back(item);
-
-                    println!(
-                        "Camion {}: dejé paquete {}", id,
-                        item
-                    );
+                println!(
+                    "Camion {}: dejé paquete {}", id,
+                    item
+                );
+                //Notifica a cualquiera de los robots que hay un producto en la cinta 
                 not_empty.notify_one();
                 drop(state);
                 
+                //Si no hay más productos termina
                 if lista.0.is_empty(){
                     drop(lista);
                     break;
@@ -63,23 +67,23 @@ pub fn zona_recepcion() {
                 drop(lista);
                     
                 thread::sleep(Duration::from_millis(rng.gen_range(10..100)));
-
             }
 
             let mut state = lock.lock().unwrap();
             state.1 = true;
+            //Avisa a todos los robots para que salgan de sus bloqueos
             not_empty.notify_all(); 
             println!("Camion {} terminó de descargar", id);
         });
-        producers.push(producer);
+        camiones.push(camion);
     }
 
-    let mut consumers = vec![];
+    let mut robots = vec![];
     for id in 1..=3 {
         
         let buffer_cons = Arc::clone(&buffer);
 
-        let consumer = thread::spawn(move || {
+        let robot = thread::spawn(move || {
             let mut rng = rand::thread_rng();
             let (ref lock, ref not_full, ref not_empty) = *buffer_cons;
 
@@ -90,6 +94,7 @@ pub fn zona_recepcion() {
                     if state.1 { println!("Robot {}: no hay más items para descargar.", id); return; }else{
                         println!("No hay nada para descargar, el robot {} espera", id);
                     }
+                    //Espera a que lo notifiquen que hay un producto en la cinta
                     state = not_empty.wait(state).unwrap();
                 }
 
@@ -99,21 +104,21 @@ pub fn zona_recepcion() {
                     "Robot {}: tomó paquete {}",
                     id, item
                 );
-
+                //Avisa a cualquiera de los camiones que ya hay lugar en la cinta
                 not_full.notify_one();
 
                 drop(state);
                 thread::sleep(Duration::from_millis(rng.gen_range(10..100)));
             }
         });
-        consumers.push(consumer);
+        robots.push(robot);
     }
 
-    for producer in producers {
-        producer.join().unwrap();
+    for camion in camiones {
+        camion.join().unwrap();
     }
     
-    for c in consumers {
-        c.join().unwrap();
+    for robot in robots {
+        robot.join().unwrap();
     }
 }
